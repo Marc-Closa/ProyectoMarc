933b252771c15a71790bf48d8ef465bbdcc06cb2
Marc-Closa <closamarc51@gmail.com>
2024-11-26 17:19:07 +0100

    añado pie de página

---
+++ footer.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html> 
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    footer {
+            text-align: center;
+            background-color: #1c1c1c;
+            color: #d4af37; /* Dorado */
+            padding: 10px 0;
+            margin-top: 20px;
+            font-size: 0.9em;
+        }
+    </style>
+
+</head>
+<body>
+
+    <footer>
+        © 2024 Restaurante Chino. Todos los derechos reservados.
+    </footer>
+    
+</body>
+</html>
