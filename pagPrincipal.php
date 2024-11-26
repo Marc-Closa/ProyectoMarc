c8562aacf72cfea2b1a76d8bb6fed43cc6636c63
Marc-Closa <closamarc51@gmail.com>
2024-11-26 23:02:05 +0100

    añado boton a página principal

---
+++ pagPrincipal.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página principal</title>
+    <style>
+    input[type="submit"]:hover {
+            background-color: #d4af37; /* Dorado */
+        }
+        .link-button {
+            display: inline-block;
+            text-align: center;
+            background-color: #d32f2f; /* Rojo elegante */
+            color: #f5f5f5; /* Texto claro */
+            text-decoration: none;
+            font-weight: bold;
+            border-radius: 4px;
+            padding: 10px 20px;
+            margin-top: 15px;
+            cursor: pointer;
+        }
+        .link-button:hover {
+            background-color: #d4af37; /* Dorado */
+        }
+    </style>
+</head>
+
+<body>
+<a href="index.php" class="link-button">Volver a la página principal</a><br>
+</body>
+</html>
