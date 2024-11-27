743c45f59217f171a2456fa125a0a8d1395793ef
Marc-Closa <closamarc51@gmail.com>
2024-11-27 17:07:09 +0100

    carrito

--- addCarrito.php
+++ addCarrito.php
@@ -1,38 +1,21 @@
-<!DOCTYPE html> 
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Carrito</title>
-    <h1>Carrito</h1>
-</head>
-<body>
 <?php
+
 session_start();
 
-include('conBaseDatos.php');
+$carrito = $_SESSION["carrito"] 
 
-$idp = $_REQUEST["id_producto"];
+if(isset($_SESSION["carrito"])){
+    include("conBaseDatos.php");
 
-// construyo la consulta
-$sql = "SELECT * FROM productos where id_producto = '$idp'";
+    $sql = "select * from usuarios";
 
-// ejecuto la consulta
-$result = $conn->query($sql);
+    $result = $conn->query($sql);
 
-// si quiero saber el numero de registros encontrados
-$row_count = $result->num_rows;
+    $row_count = $result->num_rows;
 
-// Si quiero mostrarlos o recorrerlos
-while($row = $result->fetch_assoc()) {
-    echo $row['nombre']; // en 'nombre' es el nombre de la columna de la BBDD
+    while($row = $result->fetch_assoc()) {
+        echo $row['nombre']; 
+
 }
-
-
-
-// Cierro la conexión
-$conn->close();
-
+}
 ?>
-</body>
-</head>
--- verCarrito.php
+++ verCarrito.php
@@ -1,2 +1,37 @@
-<h1>hola</h1>
-aqui iniciar sesion y ver todos los productos que han añadido al carrito
+<!DOCTYPE html> 
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrito</title>
+    <h1>Carrito</h1>
+</head>
+<body>
+<?php
+
+include('conBaseDatos.php');
+
+$idp = $_REQUEST["id_producto"];
+
+// construyo la consulta
+$sql = "SELECT * FROM productos where id_producto = '$idp'";
+
+// ejecuto la consulta
+$result = $conn->query($sql);
+
+// si quiero saber el numero de registros encontrados
+$row_count = $result->num_rows;
+
+// Si quiero mostrarlos o recorrerlos
+while($row = $result->fetch_assoc()) {
+    echo $row['nombre']; // en 'nombre' es el nombre de la columna de la BBDD
+}
+
+
+
+// Cierro la conexión
+$conn->close();
+
+?>
+</body>
+</head>
